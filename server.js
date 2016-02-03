78f76052df637ab479306b61b0cc74399d34d8b9
fugalfunkster <mtthwhsltn@gmail.com>
2016-02-02 16:45:59 -0800

    api0 first shot

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+var router = require('./routes/APIrouter');
+
+app.use('/api', router);
+
+app.get('/', function(req, res) {
+    res.send("welcome to my api");
+});
+
+app.listen(8080, function(){
+  console.log("listening on port 8080");
+});
